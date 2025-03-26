7df07d03656ec9a70c9c1770633ec6dd5dc1b846
BhabhaSeptember <septemberbhabha@gmail.com>
2025-03-26 08:43:52 +0200

    commit

--- exercises/src/main/java/lesson03/Hello.java
+++ exercises/src/main/java/lesson03/Hello.java
@@ -8,6 +8,9 @@ package lesson03;
  *
  * @author User
  */
-public class Lesson03 {
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Lesson03 - main class!");
+    }
     
 }
